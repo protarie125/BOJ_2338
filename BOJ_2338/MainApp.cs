e2588773ab292d59e18be752f5fdcdda371c65de
protarie125 <proarie@gmail.com>
2021-11-30 12:49:44 +0900

    초기 커밋.

---
+++ BOJ_2338/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace BOJ_2338
+{
+    internal static class MainApp
+    {
+        private static void Main(string[] args)
+        {
+            var l1 = Console.ReadLine();
+            var l2 = Console.ReadLine();
+
+            var a = BigInteger.Parse(l1);
+            var b = BigInteger.Parse(l2);
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+        }
+    }
+}
